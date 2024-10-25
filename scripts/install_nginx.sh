16114ecb0036b5b00244fbd0dbc0ffa7fb92bb24
dimpledhiman <>
2024-10-25 16:31:28 +0530

    Changes in post_build

--- scripts/install_nginx.sh
+++ scripts/install_nginx.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install nginx -y
+
+if ! command -v nginx &> /dev/null; then
+    echo "Nginx is not installed. Installing..."
+    sudo apt-get install nginx -y
+else
+    echo "Nginx is already installed."
+fi
